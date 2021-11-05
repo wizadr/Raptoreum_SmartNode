be49fb2ec2725dd3cbef8aaafff3ae6546d10c4d
wizadr <36131726+wizadr@users.noreply.github.com>
2021-11-05 06:07:45 +0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 DIR='/raptoreum/.raptoreumcore'
-BOOTSTRAP_TAR='https://github.com/Raptor3um/raptoreum/releases/download/1.3.15.99/testnet_bootstrap_with_indexes.zip'
+BOOTSTRAP_TAR='https://www.dropbox.com/s/y885aysstdmro4n/rtm-bootstrap.tar.gz'
 
 if [ ! -d $DIR ]; then
   mkdir -p $DIR
-  curl -L $BOOTSTRAP_TAR | unzip -C $DIR
+  curl -L $BOOTSTRAP_TAR | tar xz -C $DIR
 else
   echo "Datadir has been detected so bootstrap will not be used..."
 fi
