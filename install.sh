2f08df2c29eefca9d60d70c36fa08bb0de1e3198
wizadr <36131726+wizadr@users.noreply.github.com>
2021-11-06 14:27:37 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -70,20 +70,20 @@ function ip_confirm() {
   fi
 }
 
-function create_swap() {
-  echo -e "${YELLOW}Creating swap if none detected...${NC}" && sleep 1
-  if ! grep -q "swapfile" /etc/fstab; then
-    if whiptail --yesno "No swapfile detected would you like to create one?" 8 54; then
-      sudo fallocate -l 4G /swapfile
-      sudo chmod 600 /swapfile
-      sudo mkswap /swapfile
-      sudo swapon /swapfile
-      echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
-      echo -e "${YELLOW}Created ${SEA}4G${YELLOW} swapfile${NC}"
-    fi
-  fi
-  sleep 1
-}
+#function create_swap() {
+#  echo -e "${YELLOW}Creating swap if none detected...${NC}" && sleep 1
+#  if ! grep -q "swapfile" /etc/fstab; then
+#    if whiptail --yesno "No swapfile detected would you like to create one?" 8 54; then
+#      sudo fallocate -l 4G /swapfile
+#      sudo chmod 600 /swapfile
+#      sudo mkswap /swapfile
+#      sudo swapon /swapfile
+#      echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+#      echo -e "${YELLOW}Created ${SEA}4G${YELLOW} swapfile${NC}"
+#    fi
+#  fi
+#  sleep 1
+#}
 
 function install_packages() { 
   echo -e "${YELLOW}Installing Packages...${NC}"
@@ -169,60 +169,60 @@ function bootstrap() {
   fi
 }
 
-function update_script() {
-    echo -e "${YELLOW}Creating a script to update binaries for future updates...${NC}"
-    touch $HOME/update.sh
-    cat << EOF > $HOME/update.sh
-#!/bin/bash
-WALLET_TAR=\$(curl -s https://api.github.com/repos/wizadr/raptoreum/releases/latest | jq -r '.assets[] | select(.name|test("ubuntu18.")) | .browser_download_url')
-COIN_NAME='raptoreum'
-COIN_DAEMON='raptoreumd'
-COIN_CLI='raptoreum-cli'
-COIN_TX='raptoreum-tx'
-COIN_PATH='/usr/local/bin'
-sudo systemctl stop \$COIN_NAME
-\$COIN_CLI stop > /dev/null 2>&1 && sleep 2
-sudo killall \$COIN_DAEMON > /dev/null 2>&1
-mkdir temp
-curl -L \$WALLET_TAR | tar xz -C ./temp; sudo mv ./temp/\$COIN_DAEMON ./temp/\$COIN_CLI ./temp/\$COIN_TX \$COIN_PATH
-rm -rf temp
-sudo chmod 755 \${COIN_PATH}/\${COIN_NAME}*
-sudo systemctl start \$COIN_NAME > /dev/null 2>&1
-EOF
-    sudo chmod 775 $HOME/update.sh
-}
+#function update_script() {
+#    echo -e "${YELLOW}Creating a script to update binaries for future updates...${NC}"
+#    touch $HOME/update.sh
+#    cat << EOF > $HOME/update.sh
+##!/bin/bash
+#WALLET_TAR=\$(curl -s https://api.github.com/repos/wizadr/raptoreum/releases/latest | jq -r '.assets[] | select(.name|test("ubuntu18.")) | .browser_download_url')
+#COIN_NAME='raptoreum'
+#COIN_DAEMON='raptoreumd'
+#COIN_CLI='raptoreum-cli'
+#COIN_TX='raptoreum-tx'
+#COIN_PATH='/usr/local/bin'
+#sudo systemctl stop \$COIN_NAME
+#\$COIN_CLI stop > /dev/null 2>&1 && sleep 2
+#sudo killall \$COIN_DAEMON > /dev/null 2>&1
+#mkdir temp
+#curl -L \$WALLET_TAR | tar xz -C ./temp; sudo mv ./temp/\$COIN_DAEMON ./temp/\$COIN_CLI ./temp/\$COIN_TX \$COIN_PATH
+#rm -rf temp
+#sudo chmod 755 \${COIN_PATH}/\${COIN_NAME}*
+#sudo systemctl start \$COIN_NAME > /dev/null 2>&1
+#EOF
+#    sudo chmod 775 $HOME/update.sh
+#}
 
-function create_service() {
-  echo -e "${YELLOW}Creating tRTM service...${NC}"
-  sudo touch /etc/systemd/system/$COIN_NAME.service
-  sudo chown $USERNAME:$USERNAME /etc/systemd/system/$COIN_NAME.service
-  cat << EOF > /etc/systemd/system/$COIN_NAME.service
-[Unit]
-Description=$COIN_NAME service
-After=network.target
-[Service]
-Type=forking
-User=$USERNAME
-Group=$USERNAME
-WorkingDirectory=/home/$USERNAME/$CONFIG_DIR/
-ExecStart=$COIN_PATH/$COIN_DAEMON -datadir=/home/$USERNAME/$CONFIG_DIR/ -conf=/home/$USERNAME/$CONFIG_DIR/$CONFIG_FILE -daemon
-ExecStop=$COIN_PATH/$COIN_CLI stop
-Restart=always
-RestartSec=3
-PrivateTmp=true
-TimeoutStopSec=60s
-TimeoutStartSec=10s
-StartLimitInterval=120s
-StartLimitBurst=5
-[Install]
-WantedBy=multi-user.target
-EOF
-  sudo chown root:root /etc/systemd/system/$COIN_NAME.service
-  sudo systemctl daemon-reload
-  sudo systemctl enable $COIN_NAME > /dev/null 2>&1
-}
+#function create_service() {
+#  echo -e "${YELLOW}Creating tRTM service...${NC}"
+#  sudo touch /etc/systemd/system/$COIN_NAME.service
+#  sudo chown $USERNAME:$USERNAME /etc/systemd/system/$COIN_NAME.service
+#  cat << EOF > /etc/systemd/system/$COIN_NAME.service
+#[Unit]
+#Description=$COIN_NAME service
+#After=network.target
+#[Service]
+#Type=forking
+#User=$USERNAME
+#Group=$USERNAME
+#WorkingDirectory=/home/$USERNAME/$CONFIG_DIR/
+#ExecStart=$COIN_PATH/$COIN_DAEMON -datadir=/home/$USERNAME/$CONFIG_DIR/ -conf=/home/$USERNAME/$CONFIG_DIR/$CONFIG_FILE -daemon
+#ExecStop=$COIN_PATH/$COIN_CLI stop
+#Restart=always
+#RestartSec=3
+#PrivateTmp=true
+#TimeoutStopSec=60s
+#TimeoutStartSec=10s
+#StartLimitInterval=120s
+#StartLimitBurst=5
+#[Install]
+#WantedBy=multi-user.target
+#EOF
+#  sudo chown root:root /etc/systemd/system/$COIN_NAME.service
+#  sudo systemctl daemon-reload
+#  sudo systemctl enable $COIN_NAME > /dev/null 2>&1
+#}
 
-SECURITY_ANS=""
+#SECURITY_ANS=""
 # If $1 is provided, just ask about bootstrap
 #function basic_security() {
 #  if [[ ! -z $1 ]]; then
@@ -257,23 +257,23 @@ SECURITY_ANS=""
 #  fi
 #}
 
-function start_daemon() {
-  NUM='180'
-  MSG1='Starting daemon service & syncing chain please be patient this will take few min...'
-  MSG=''
-  if sudo systemctl start $COIN_NAME > /dev/null 2>&1; then
-    echo && spinning_timer
-    NUM='5'
-    MSG1='Getting blockchain info...'
-    MSG2=''
-    echo && spinning_timer
-    echo
-    $COIN_CLI getblockchaininfo
-  else
-    echo -e "${RED}Something is not right the daemon did not start. Will exit out so try and run the script again.${NC}"
-    exit
-  fi
-}
+#function start_daemon() {
+#  NUM='180'
+#  MSG1='Starting daemon service & syncing chain please be patient this will take few min...'
+#  MSG=''
+#  if sudo systemctl start $COIN_NAME > /dev/null 2>&1; then
+#    echo && spinning_timer
+#    NUM='5'
+#    MSG1='Getting blockchain info...'
+#    MSG2=''
+#    echo && spinning_timer
+#    echo
+#    $COIN_CLI getblockchaininfo
+#  else
+#    echo -e "${RED}Something is not right the daemon did not start. Will exit out so try and run the script again.${NC}"
+#    exit
+#  fi
+#}
 
 #function log_rotate() {
 #  echo -e "${YELLOW}Configuring logrotate function for debug log...${NC}"
@@ -295,83 +295,83 @@ function start_daemon() {
 #  sudo chown root:root /etc/logrotate.d/rtmdebuglog
 #}
 
-CRON_ANS=""
-PROTX_HASH=""
+#CRON_ANS=""
+#PROTX_HASH=""
 # If $1 is provided, just ask about bootstrap.
-function cron_job() {
-  if [[ ! -z $1 ]]; then
-    if whiptail --yesno "Would you like Cron to check on daemon's health every 15 minutes?" 8 63; then
-      CRON_ANS=1
-      PROTX_HASH=$(whiptail --inputbox "Please enter your protx hash for this SmartNode" 8 51 3>&1 1>&2 2>&3)
-    fi
-  elif [[ ! -z $CRON_ANS ]]; then
-    cat <(curl -s https://raw.githubusercontent.com/wizadr/Raptoreum_Smartnode/main/check.sh) >$HOME/check.sh
-    sed -i "s/#NODE_PROTX=/NODE_PROTX=\"${PROTX_HASH}\"/g" $HOME/check.sh
-    sudo chmod 775 $HOME/check.sh
-    crontab -l | grep -v "SHELL=/bin/bash" | crontab -
-    crontab -l | grep -v "RAPTOREUM_CLI=$(which $COIN_CLI)" | crontab -
-    crontab -l | grep -v "HOME=$HOME" | crontab -
-    crontab -l | grep -v "$HOME/check.sh >> $HOME/check.log" | crontab -
-    crontab -l > tempcron
-    echo "SHELL=/bin/bash" >> tempcron
-    echo "RAPTOREUM_CLI=$(which $COIN_CLI)" >> tempcron
-    echo "HOME=$HOME" >> tempcron
-    echo "*/15 * * * * $HOME/check.sh >> $HOME/check.log" >> tempcron
-    crontab tempcron
-    rm tempcron
-    rm -f /tmp/height 2>/dev/null
-    rm -f /tmp/pose_score 2>/dev/null
-    rm -f /tmp/was_stuck 2>/dev/null
-    rm -f /tmp/prev_stuck 2>/dev/null
-  fi
-}
+#function cron_job() {
+#  if [[ ! -z $1 ]]; then
+#    if whiptail --yesno "Would you like Cron to check on daemon's health every 15 minutes?" 8 63; then
+#      CRON_ANS=1
+#      PROTX_HASH=$(whiptail --inputbox "Please enter your protx hash for this SmartNode" 8 51 3>&1 1>&2 2>&3)
+#    fi
+#  elif [[ ! -z $CRON_ANS ]]; then
+#    cat <(curl -s https://raw.githubusercontent.com/wizadr/Raptoreum_Smartnode/main/check.sh) >$HOME/check.sh
+#    sed -i "s/#NODE_PROTX=/NODE_PROTX=\"${PROTX_HASH}\"/g" $HOME/check.sh
+#    sudo chmod 775 $HOME/check.sh
+#    crontab -l | grep -v "SHELL=/bin/bash" | crontab -
+#    crontab -l | grep -v "RAPTOREUM_CLI=$(which $COIN_CLI)" | crontab -
+#    crontab -l | grep -v "HOME=$HOME" | crontab -
+#    crontab -l | grep -v "$HOME/check.sh >> $HOME/check.log" | crontab -
+#    crontab -l > tempcron
+#    echo "SHELL=/bin/bash" >> tempcron
+#    echo "RAPTOREUM_CLI=$(which $COIN_CLI)" >> tempcron
+#    echo "HOME=$HOME" >> tempcron
+#    echo "*/15 * * * * $HOME/check.sh >> $HOME/check.log" >> tempcron
+#    crontab tempcron
+#    rm tempcron
+#    rm -f /tmp/height 2>/dev/null
+#    rm -f /tmp/pose_score 2>/dev/null
+#    rm -f /tmp/was_stuck 2>/dev/null
+#    rm -f /tmp/prev_stuck 2>/dev/null
+#  fi
+#}
 
-function create_motd() {
-  touch $HOME/99-smartnode
-  sudo cat << EOF >$HOME/99-smartnode
-#!/bin/bash
-COIN_NAME='raptoreum'
-COIN_CLI='raptoreum-cli'
+#function create_motd() {
+#  touch $HOME/99-smartnode
+#  sudo cat << EOF >$HOME/99-smartnode
+##!/bin/bash
+#COIN_NAME='raptoreum'
+#COIN_CLI='raptoreum-cli'
 
-#color codes
-RED='\033[1;31m'
-YELLOW='\033[1;33m'
-BLUE="\\033[38;5;27m"
-SEA="\\033[38;5;49m"
-GREEN='\033[1;32m'
-CYAN='\033[1;36m'
-BLINKRED='\033[1;31;5m'
-NC='\033[0m'
-STOP='\e[0m'
-X_POINT="\${BLINKRED}\xE2\x9D\x97\${NC}"
-
-printf "\${BLUE}"
-figlet -t -k "tRTM  SMARTNODES"
-printf "\${STOP}"
-
-#echo -e "\${YELLOW}================================================================================================"
-#echo -e "\${CYAN}COURTESY OF DK808 FROM ALTTANK ARMY\${NC}"
-echo -e "\${CYAN}Smartnode healthcheck by Delgon\${NC}"
-echo
-echo -e "\${YELLOW}Commands to manage \$COIN_NAME service\${NC}"
-echo -e "  TO START- \${CYAN}sudo systemctl start \$COIN_NAME\${NC}"
-echo -e "  TO STOP - \${CYAN}sudo systemctl stop \$COIN_NAME\${NC}"
-echo -e "  STATUS  - \${CYAN}sudo systemctl status \$COIN_NAME\${NC}"
-echo -e "In the event server \${RED}reboots\${NC} daemon service will \${GREEN}auto-start\${NC}"
-echo
-echo -e "\${X_POINT}\${X_POINT} \${YELLOW}To use \$COIN_CLI simply start command with \$COIN_CLI" \${X_POINT}\${X_POINT}
-echo -e "     \${YELLOW}E.g \${CYAN}\$COIN_CLI getblockchaininfo\${NC}"
-echo -e "     \${YELLOW}E.g \${CYAN}\$COIN_CLI smartnode status\${NC}"
-echo
-echo -e "\${YELLOW}To update binaries when new ones are released enter \${SEA}./update.sh\${NC}"
-echo -e "\${YELLOW}================================================================================================\${NC}"
-EOF
-  sudo chmod 775 $HOME/99-smartnode
-  bash $HOME/99-smartnode
-  sudo cp $HOME/99-smartnode /etc/update-motd.d 2>/dev/null
-  sudo chown root:root /etc/update-motd.d/99-smartnode 2>/dev/null
-  rm $HOME/99-smartnode
-}
+##color codes
+#RED='\033[1;31m'
+#YELLOW='\033[1;33m'
+#BLUE="\\033[38;5;27m"
+#SEA="\\033[38;5;49m"
+#GREEN='\033[1;32m'
+#CYAN='\033[1;36m'
+#BLINKRED='\033[1;31;5m'
+#NC='\033[0m'
+#STOP='\e[0m'
+#X_POINT="\${BLINKRED}\xE2\x9D\x97\${NC}"
+#
+#printf "\${BLUE}"
+#figlet -t -k "tRTM  SMARTNODES"
+#printf "\${STOP}"
+#
+##echo -e "\${YELLOW}================================================================================================"
+##echo -e "\${CYAN}COURTESY OF DK808 FROM ALTTANK ARMY\${NC}"
+#echo -e "\${CYAN}Smartnode healthcheck by Delgon\${NC}"
+#echo
+#echo -e "\${YELLOW}Commands to manage \$COIN_NAME service\${NC}"
+#echo -e "  TO START- \${CYAN}sudo systemctl start \$COIN_NAME\${NC}"
+#echo -e "  TO STOP - \${CYAN}sudo systemctl stop \$COIN_NAME\${NC}"
+#echo -e "  STATUS  - \${CYAN}sudo systemctl status \$COIN_NAME\${NC}"
+#echo -e "In the event server \${RED}reboots\${NC} daemon service will \${GREEN}auto-start\${NC}"
+#echo
+#echo -e "\${X_POINT}\${X_POINT} \${YELLOW}To use \$COIN_CLI simply start command with \$COIN_CLI" \${X_POINT}\${X_POINT}
+#echo -e "     \${YELLOW}E.g \${CYAN}\$COIN_CLI getblockchaininfo\${NC}"
+#echo -e "     \${YELLOW}E.g \${CYAN}\$COIN_CLI smartnode status\${NC}"
+#echo
+#echo -e "\${YELLOW}To update binaries when new ones are released enter \${SEA}./update.sh\${NC}"
+#echo -e "\${YELLOW}================================================================================================\${NC}"
+#EOF
+#  sudo chmod 775 $HOME/99-smartnode
+#  bash $HOME/99-smartnode
+#  sudo cp $HOME/99-smartnode /etc/update-motd.d 2>/dev/null
+#  sudo chown root:root /etc/update-motd.d/99-smartnode 2>/dev/null
+#  rm $HOME/99-smartnode
+#}
 
 #
 #end of functions
@@ -380,23 +380,23 @@ EOF
   wipe_clean
 
 # Ask about about things first for quick setup.
-  ssh_port
+ # ssh_port
   ip_confirm
-  create_swap
+ # create_swap
   create_conf true
 # basic_security true
   bootstrap true
-  cron_job true
+#  cron_job true
 
 #Run functionis.
   install_packages
   install_bins
   create_conf
   bootstrap
-  create_service
+#  create_service
 # basic_security
-  start_daemon
-  cron_job
+#  start_daemon
+#  cron_job
 #  log_rotate
-  update_script
-  create_motd
+#  update_script
+#  create_motd
